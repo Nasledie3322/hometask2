a7583108523b868e9b02bd8c1f0351070bb75393
Umed <majidowumed@gmail.com>
2025-08-09 15:01:02 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+/*task1
+int Fact(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return n * Fact(n - 1);
+    }
+}
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Fact(n));*/
+
+
+/*task2
+int Fiba(int n) {
+    if (n <= 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fiba(n - 1) + Fiba(n - 2);
+    }
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Fiba(n));*/
+
+
+
+/*task3
+int DoubleFactorial(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return n * DoubleFactorial(n - 2);
+    }
+}
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(DoubleFactorial(n));*/
